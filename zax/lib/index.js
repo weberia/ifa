4594c8d1e5d0b8dd199b86a6e89236548496d591
Bambang Purnomosidi D. P <bambangpdp@yahoo.com>
2016-01-17 14:47:41 +0700

    chain trim to this.grammar

--- zax/lib/index.js
+++ zax/lib/index.js
@@ -4,11 +4,11 @@ var	fs = require('fs'),
     PEG = require('pegjs');
 
 function Zax() {
-  this.grammar = fs.readFileSync(__dirname + '/ifa.pegjs', 'utf8').toString();
+  this.grammar = fs.readFileSync(__dirname + '/ifa.pegjs', 'utf8').toString().trim();
 };
 
 Zax.prototype.query = function (statement) {
-  var parser = PEG.buildParser(this.grammar.trim());
+  var parser = PEG.buildParser(this.grammar);
   return parser.parse(statement.trim());
 }
 
