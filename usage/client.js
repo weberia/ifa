1789af7869fbed8377d6d37030d0737c566a0df7
Bambang Purnomosidi D. P <bambangpdp@yahoo.com>
2016-04-05 02:54:10 +0700

    ---

--- usage/client.js
+++ usage/client.js
@@ -16,26 +16,25 @@ function isJson(str) {
     return true;
 }
 
-/*
-request('http://archera:8000/resources.json', function (error, response, body) {
+var qR1 = 'REQUEST "http://archera:8000/speax" "null"';
+var qR1Sent = zax.query(qR1);
+request('http://archera:8000/speax/' + qR1 , function (error, response, bodyR1) {
+  console.log('request ' + qR1 + '...');
   if (!error && response.statusCode == 200) {
-    var resourcesjson = JSON.parse(body);
-    console.log(resourcesjson);
-    console.log(resourcesjson[0].resources.semantic);
-  }
-});
-*/
-
-var q1 = 'REQUEST "http://archera:8000/speax" "null"';
-var q1Sent = zax.query(q1);
-request('http://archera:8000/speax/' + q1 , function (error, response, body) {
-  console.log('request ' + q1 + '...');
-  if (!error && response.statusCode == 200) {
-    var resourcesjson = JSON.parse(body);
-    console.log(resourcesjson);
+    var jsonRequest1 = JSON.parse(bodyR1);
+    console.log(jsonRequest1);
   }
 });
 
+var qA1 = 'ASSERT "http://archera:8000/speax" "9781617292019 > 0"';
+var qA1Sent = zax.query(qA1);
+request('http://archera:8000/speax/' + qA1 , function (error, response, bodyA1) {
+  console.log('request ' + qA1 + '...');
+  if (!error && response.statusCode == 200) {
+    var jsonAssert1 = JSON.parse(bodyA1);
+    console.log(jsonAssert1);
+  }
+});
 
 
 /*
